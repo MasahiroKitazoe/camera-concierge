5d473d7464b2bc259af26f874fa9aa1d11440f0e
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-28 13:22:48 +0900

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 require 'csv'
 
-# csv_data = CSV.read('db/camera_types.csv', headers: true)
-# csv_data.each do |data|
-#   CameraType.create!(data.to_hash)
-# end
+csv_data = CSV.read('db/camera_types.csv', headers: true)
+csv_data.each do |data|
+  CameraType.create!(data.to_hash)
+end
 
-# csv_data = CSV.read('db/finders.csv', headers: true)
-# csv_data.each do |data|
-#   Finder.create!(data.to_hash)
-# end
+csv_data = CSV.read('db/finders.csv', headers: true)
+csv_data.each do |data|
+  Finder.create!(data.to_hash)
+end
 
-# csv_data = CSV.read('db/frames.csv', headers: true)
-# csv_data.each do |data|
-#   Frame.create!(data.to_hash)
-# end
+csv_data = CSV.read('db/frames.csv', headers: true)
+csv_data.each do |data|
+  Frame.create!(data.to_hash)
+end
 
-# csv_data = CSV.read('db/makers.csv', headers: true)
-# csv_data.each do |data|
-#   Maker.create!(data.to_hash)
-# end
+csv_data = CSV.read('db/makers.csv', headers: true)
+csv_data.each do |data|
+  Maker.create!(data.to_hash)
+end
 
 csv_data = CSV.read('db/cameras.csv', headers: true)
 csv_data.each do |data|
